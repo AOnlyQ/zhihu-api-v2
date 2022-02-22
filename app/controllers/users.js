e1de0fa145d17a749ae2651850b78239a4ae01b6
AONLY <atiffiany@outlook.com>
2022-02-22 16:56:34 +0800

    feat:commit error handle

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,20 +1,53 @@
+const { required } = require("nodemon/lib/config")
+
 const db = [{ name: '李雷' }, { name: "张三" }]
 class UserCtrl {
   find (ctx) {
+    a.b = 3
     ctx.body = db
   }
   findId (ctx) {
+    if (+ctx.params.id >= db.length) {
+      ctx.throw(412, '先决条件失败：id大于数组条件长度')
+    }
     ctx.body = db[+ctx.params.id]
   }
   create (ctx) {
+    ctx.verifyParams({
+      name: {
+        type: 'string',
+        required: true
+      },
+      age: {
+        type: 'number',
+        required: false
+      }
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
   update (ctx) {
+    if (ctx.params.id >= db.length) {
+      ctx.throw(412, '先决条件失败：id大于数组条件长度')
+    }
+    ctx.verifyParams({
+      name: {
+        type: 'string',
+        required: true
+      },
+      age: {
+        type: 'number',
+        required: false
+      }
+    })
+
     db[+ctx.params.id] = ctx.request.body
     ctx.body = ctx.request.body
   }
   delete (ctx) {
+    if (ctx.params.id >= db.length) {
+      ctx.throw(412, '先决条件失败：id大于数组条件长度')
+    }
     db.splice(+ctx.params.id, 1)
     ctx.status = 204 //没有内容，但是成功了
   }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 const Koa = require("koa")
 const bodyparser = require('koa-bodyparser')
+const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const app = new Koa();
 const routing = require('./routes')
 
+app.use(error({
+  // 后置的修改返回格式,生产环境不暴露堆栈信息
+  postFormat: (err, { stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
+}))
+
 app.use(bodyparser())
+app.use(parameter(app))
 routing(app)
 
 app.listen(3000, () => {
